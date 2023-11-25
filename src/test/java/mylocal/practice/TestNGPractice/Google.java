b817ac74f4f759032a835a2f4d9e87244d55df7f
RameshAutomation <sripvrameshreddy@gmail.com>
2023-11-26 00:46:58 +0530

    First commit doing in Google file

--- src/test/java/mylocal/practice/TestNGPractice/Google.java
+++ src/test/java/mylocal/practice/TestNGPractice/Google.java
@@ -1,6 +1,10 @@
 package mylocal.practice.TestNGPractice;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,11 +16,15 @@ public class Google {
 	public void getPageTitle() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.amazon.com");
 		 driver.manage().timeouts().implicitlyWait(java.time.Duration.ofMinutes(1));
 		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println(title);
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+		
+		// close all browser and sessions
 		driver.quit();
 	}
 
